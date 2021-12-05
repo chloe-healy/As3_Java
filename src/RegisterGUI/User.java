da7bf51a238dada07020363f1f369df190eeabdf
chloe <118720535@umail.ucc.ie>
2021-12-05 12:38:38 +0000

    Created Password tests

--- src/RegisterGUI/User.java
+++ src/RegisterGUI/User.java
@@ -15,7 +15,59 @@ public class User {
         this.email = email;
         this.password = password;
 
-        public boolean verifyEmail (String email){
+        public boolean verifyEmail(String testEmail) {
+            String emailR = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                    + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+            return testEmail.matches(emailR);
         }
     }
+
+    /**
+     * Function to verify if the password is the correct length
+     * @param testPassword string representation of password to be compared
+     * @return password if it has 7 or more charachters
+     */
+    public boolean verifyLength(String testPassword) {
+        return testPassword.length() >= 7;
+    }
+
+    /**
+     * Function to verify if the password contains special charachters
+     * @param testPassword string representation of password to be compared
+     * @return password that contains a special charachter
+     */
+    public boolean verifySpecial(String testPassword) {
+        String specialR = ".*[/^&@!.].*";
+        return password.matches(specialR);
+    }
+
+    /**
+     * Function to verify if the password contains a number
+     * @param testPassword string representation of password to be compared
+     * @return  user passwords that contains a number
+     */
+    public boolean verifyNum(String testPassword) {
+        String numR = ".*[0-9].*";
+        return password.matches(numR);
+    }
+
+    /**
+     * Function to verfity that a password contains a letter
+     * @param testPassword string representation of password to be compared
+     * @return password that contains a letter
+     */
+    public boolean verifyAlpha(String testPassword) {
+        String alphaR = ".*[a-zA-Z].*";
+        return password.matches(alphaR);
+    }
+
+    /**
+     * Function to verify the password, based on all the functions above
+     * @param testPassword string representation of password to be compared
+     * @return verified password
+     */
+    public boolean verifyPass(String testPassword) {
+        return (verifyLength(testPassword) && verifySpecial(testPassword) && verifyNum(testPassword) && verifyAlpha(testPassword));
+    }
 }
+
--- src/Test/test.java
+++ src/Test/test.java
@@ -17,4 +17,11 @@ public class test {
         assertFalse(usr.verifyEmail(testEmail));
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"student123@umail.ucc.ie", "john@gmail.com", "tina99@hotmail.com"})
+    public void validEmailTest(String testEmail) {
+        User usr = new User(testEmail, userPass);
+        assertTrue(usr.verifyEmail(testEmail));
+    }
+
 }
