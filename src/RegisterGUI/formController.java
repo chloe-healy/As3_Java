7ab8f80ebab7549c24ecb7e5899f734cca901ba0
chloe <118720535@umail.ucc.ie>
2021-12-05 12:39:38 +0000

    Added Alerts to controller

--- src/RegisterGUI/formController.java
+++ src/RegisterGUI/formController.java
@@ -20,6 +20,32 @@ public class formController {
         String password = pass.getText();
         String email = emailField.getText();
         User usr = new User(email, password);
+
+        if (usr.verifyEmail(email) && usr.verifyPass(password)) {
+            showAlert(Alert.AlertType.CONFIRMATION, "Registration Success!",
+                    "You have been registered successfully!");
+
+        } else if (usr.verifyEmail(email) && !usr.verifyPass(password)) {
+            showAlert(Alert.AlertType.ERROR, "Registration Error",
+                    "Password not accepted. Try again!");
+
+        } else if (!usr.verifyEmail(email) && usr.verifyPass(password)) {
+            showAlert(Alert.AlertType.ERROR, "Registration Error",
+                    "Email invalid! Please enter a valid email address.");
+        } else {
+            showAlert(Alert.AlertType.ERROR, "Registration Error",
+                    "Invalid email. Password not accepted. Try again!");
+        }
+    }
+
+    private static void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.show();
+    }
+}
     }
 }
 
