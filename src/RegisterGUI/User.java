3631db971c23795ad996edf067b4dfd3c15e13bd
chloe <118720535@umail.ucc.ie>
2021-12-05 12:42:17 +0000

    Fixed error in User

--- src/RegisterGUI/User.java
+++ src/RegisterGUI/User.java
@@ -4,23 +4,18 @@ public class User {
     private final String email;
     private final String password;
 
-    /**
-     * Constructor to create a user object
-     *
-     * @param email    - string representation of a users email
-     * @param password - string representation of a users password
-     */
 
     public User(String email, String password) {
         this.email = email;
         this.password = password;
+    }
 
         public boolean verifyEmail(String testEmail) {
             String emailR = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
                     + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
             return testEmail.matches(emailR);
         }
-    }
+
 
     /**
      * Function to verify if the password is the correct length
