dbdeaa6696886c98c75b23c0de87388b3b9ecb3f
chloe <118720535@umail.ucc.ie>
2021-12-05 12:34:39 +0000

    Created test to verify email, failed

--- src/RegisterGUI/User.java
+++ src/RegisterGUI/User.java
@@ -5,8 +5,8 @@ public class User {
     private final String password;
 
     /**
-     *
      * Constructor to create a user object
+     *
      * @param email    - string representation of a users email
      * @param password - string representation of a users password
      */
@@ -14,5 +14,8 @@ public class User {
     public User(String email, String password) {
         this.email = email;
         this.password = password;
+
+        public boolean verifyEmail (String email){
+        }
     }
 }
--- src/Test/test.java
+++ src/Test/test.java
@@ -3,7 +3,18 @@ package Test;
 import RegisterGUI.User;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class test {
+
+    public static final String  userEmail = "email";
+    public static final String userPass = "pass";
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "gmail.com", "abc", "user@yahoo" })
+    public void invalidEmailTest(String testEmail) {
+        User usr = new User(testEmail, userPass);
+        assertFalse(usr.verifyEmail(testEmail));
+    }
 
 }
