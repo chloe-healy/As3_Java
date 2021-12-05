275136549cbb5db6e1295e764347277bea004534
chloe <118720535@umail.ucc.ie>
2021-12-05 12:25:38 +0000

    Added Event Listener to controller Created User class

--- src/RegiterGUI/formController.java
+++ src/RegiterGUI/formController.java
@@ -1,4 +1,25 @@
 package RegiterGUI;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class formController {
-}
+
+    @FXML
+    TextField emailField;
+
+    @FXML
+    PasswordField pass;
+
+    @FXML
+    Button btn;
+
+
+
+    public void register(ActionEvent actionEvent) {
+        String password = pass.getText();
+        String email = emailField.getText();
+        User usr = new User(email, password);
+    }
+
