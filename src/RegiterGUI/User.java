558f2578cf89d3abf7fc52bef6f30810159cdcfa
chloe <118720535@umail.ucc.ie>
2021-12-05 12:26:52 +0000

    Created user constructor

--- src/RegiterGUI/User.java
+++ src/RegiterGUI/User.java
@@ -1,4 +1,18 @@
 package RegiterGUI;
 
 public class User {
+    private final String email;
+    private final String password;
+
+    /**
+     * Constructor for a user object
+     *
+     * @param email    - string representation of a users email
+     * @param password - string representation of a users password
+     */
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
